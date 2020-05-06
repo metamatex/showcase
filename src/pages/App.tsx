44cd819c9d4b9bf63a33ae6d95036e0be2f689dd
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-05-06 15:32:05 +0200

    .

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,21 +6,25 @@ interface Props {
 }
 
 export const App: React.FC<Props> = (p: React.PropsWithChildren<Props>) => {
+  React.useEffect(() => {
+    document.title = p.title;
+  });
+
   return <span>
     <nav className="navbar navbar-expand-lg navbar-light" style={{"background": p.navColor}}>
-      <a className="navbar-brand" href="#">{p.title}</a>
+      <a className="navbar-brand" href="/" style={{"color": "#fff"}}>
+        <img src="/white_transparent.svg" width="30" height="30" className="d-inline-block align-top"
+             alt=""/>
+        &nbsp;&nbsp;{p.title}
+      </a>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <a className="nav-link" href="#" target="_blank" rel="noopener">Website</a>
+          <a className="nav-link" href="https://metamate.io" target="_blank" rel="noopener">Website</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="https://github.com/metamatex/metamate" target="_blank" rel="noopener">Github</a>
         </li>
       </ul>
-      <a className="navbar-brand" href="#" style={{"color": "#fff", "float": "right"}}>built with&nbsp;&nbsp;
-        <img src="/white_transparent.svg" width="30" height="30" className="d-inline-block align-top"
-             alt=""/>
-      </a>
     </nav>
     <div className="container-fluid" style={{"paddingTop": "15px"}}>
           {p.children}
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export const Index: React.FC = () => {
   return <div className="row">
-    <a href="/hackernews-user-activity">HackerNews User Activity</a>
+    <div className="col-12">
+      <a href="/hackernews-user-activity">HackerNews User Activity</a>
+    </div>
   </div>
 };
